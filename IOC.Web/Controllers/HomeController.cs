2a352a0c1b22c12a35cdb93fb9af50e54c4b8c09
mehmetsukrukavak <mehmet.kavak@dgpaysit.com>
2023-06-04 15:00:53 +0300

    Singleton, Scoped and Trancient

---
+++ IOC.Web/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using IOC.Web.Models;
+
+namespace IOC.Web.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ISingletonDateService _singletonDateService;
+    private readonly IScopedDateService _scopedDateService;
+    private readonly ITransientDateService _transientDateService;
+
+    public HomeController(ISingletonDateService singletonDateService,
+                          IScopedDateService scopedDateService,
+                          ITransientDateService transientDateService)
+    {
+        _singletonDateService = singletonDateService;
+        _scopedDateService = scopedDateService;
+        _transientDateService = transientDateService;
+    }
+
+    public IActionResult Index([FromServices] ISingletonDateService singletonDateService,
+                               [FromServices] IScopedDateService scopedDateService,
+                               [FromServices] ITransientDateService transientDateService)
+    {
+        ViewBag.time1 = _singletonDateService.GetDateTime.TimeOfDay.ToString();
+        ViewBag.time2 = singletonDateService.GetDateTime.TimeOfDay.ToString();
+
+        ViewBag.time3 = _scopedDateService.GetDateTime.TimeOfDay.ToString();
+        ViewBag.time4 = scopedDateService.GetDateTime.TimeOfDay.ToString();
+
+        ViewBag.time5 = _transientDateService.GetDateTime.TimeOfDay.ToString();
+        ViewBag.time6 = transientDateService.GetDateTime.TimeOfDay.ToString();
+
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
---
+++ IOC.Web/Models/DateService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace IOC.Web.Models
+{
+	public class DateService: ISingletonDateService,IScopedDateService,ITransientDateService
+    {
+
+		private ILogger<DateService> _logger;
+
+        public DateService(ILogger<DateService> logger)
+        {
+            _logger = logger;
+            _logger.LogWarning("DateService Constructor'ına girildi");
+        }
+
+        public DateTime GetDateTime { get; } = DateTime.Now;
+
+
+	}
+}
+
